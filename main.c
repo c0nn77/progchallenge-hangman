cc29206c31d32bb5ad1f96f1802e359248a18d82
c0nn77 <c0nn@disroot.org>
2019-08-21 19:29:39 -0300

    first commit, main.c setup

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.io.h>
+
+int main(void) {
+
+	printf("Programming Challenge 1 - Hangman\n")
+
+	// Função para pedir e guardar a palavra escolhida pelo usuário
+
+	// Ambiente da forca com o número de espaços correspondentes ao da palavra
+
+	// Função para pedir a letra escolhida pelo usuário e comparar com a palavra-chave
+
+	// Função que determina o fim do jogo, e se o jogador venceu ou não
+
+}
