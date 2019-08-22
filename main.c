97d1b14172ba3f7da4744222c5b81a2b0335b3ef
c0nn77 <c0nn@disroot.org>
2019-08-22 17:57:22 -0300

    setting up data structures and types

--- main.c
+++ main.c
@@ -1,54 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 // DECLARATIONS
-char *keyword;
-char *user_char;
-char *test_char;
+char keyword[] = "tyler";
+char user_char = 't'; 
 void start_menu(void);
-bool compare_chars(char *a, char *b);
+void compare_chars(char choice, char key[]);
 
 // MAIN
 int main(void) {
 
-	keyword = "tyler";
-	user_char = "e";
-	test_char = "d";
-
 	start_menu();
-	compare_chars(user_char, test_char);		
+  compare_chars(user_char, keyword);		
 
 }
 
 // FUNCTIONS
 
-// Create the Start Menu of the game
-
-  void start_menu(void) {
-
+// Create the Start Menu Template
+void start_menu(void) {
+	system("clear");
+	
+	for(int i = 0; i < 35; i++) {
+		printf("#");
+	}	
+	printf("\n");
 	printf("Programming Challenge 1 - Hangman\n");
-	printf("Chosen char: '%s'.\n", user_char);
-	printf("Chosen keyword: '%s'.\n", keyword);	
-
+	printf("(Developed by c0nn)\n");	
+	for(int i = 0; i < 35; i++) {
+		printf("#");
+	}
+	printf("\n");
 }
 
 // Prompt the user for the keyword and put it in a variable
 
 // Create the hang interface and prompt the user for the char
 
-// Compare the chosen char with the chars in the keyword
-
-// Handle the success or failure of the choice
-
-// Reset the round or finish the game
-
-
 // Comparing the user char with the keyword
-bool compare_chars(char *a, char *b) {
-
-	if(a != b) {
-		printf("They are different\n");
-	}
+void compare_chars(char choice, char key[]) {
+	
+	printf("User choice: %c\n", choice);
+	printf("Keyword: %s\n", key);
 
 }
+
+// Handle the success or failure of the user choice
+
+// Reset the round or finish the game
