50fb134a64020f9860862e13d0a9cee2cb13afb0
c0nn77 <c0nn@disroot.org>
2019-08-22 02:56:46 -0300

    building a start menu template and doing some tests with the compare the chars function

--- main.c
+++ main.c
@@ -2,38 +2,53 @@
 #include <string.h>
 #include <stdbool.h>
 
+// DECLARATIONS
+char *keyword;
+char *user_char;
+char *test_char;
+void start_menu(void);
+bool compare_chars(char *a, char *b);
+
+// MAIN
 int main(void) {
 
-	printf("Programming Challenge 1 - Hangman\n");
+	keyword = "tyler";
+	user_char = "e";
+	test_char = "d";
 
-	// Valores de teste
-	char *keyword  = "tyler";
-	char *c = "e";
-
-	printf("Chosen char:'%s'. \n", c);
-
-	// Função para pedir e guardar a palavra escolhida pelo usuário
-		
-
-
-	// Ambiente da forca com o número de espaços correspondentes ao da palavra
-	
-
-	// Função que determina o fim do jogo, e se o jogador venceu ou não
+	start_menu();
+	compare_chars(user_char, test_char);		
 
 }
 
+// FUNCTIONS
+
+// Create the Start Menu of the game
+
+  void start_menu(void) {
+
+	printf("Programming Challenge 1 - Hangman\n");
+	printf("Chosen char: '%s'.\n", user_char);
+	printf("Chosen keyword: '%s'.\n", keyword);	
+
+}
+
+// Prompt the user for the keyword and put it in a variable
+
+// Create the hang interface and prompt the user for the char
+
+// Compare the chosen char with the chars in the keyword
+
+// Handle the success or failure of the choice
+
+// Reset the round or finish the game
+
+
 // Comparing the user char with the keyword
-bool compare_chars(char a, char *b) {
-		
-		int n = strlen(b);
-		for (int i = 0; i < n; i++) {
-		
-			if(a != b[i]) {
-			
-				return 0;
-	
-			}
-		} 
+bool compare_chars(char *a, char *b) {
+
+	if(a != b) {
+		printf("They are different\n");
+	}
 
 }
